5e232377a215a3ddd89f27283e03ce452c4611df
Isaac Bernal <isaacb1598@gmail.com>
2023-09-08 10:50:04 -0500

    time & Speed exercise

--- js/javascript-with-HTML.js
+++ js/javascript-with-HTML.js
@@ -6,3 +6,12 @@
 // } else {
 //     console.log("boooo");
 // }
+let currentSpeed = prompt("How fast are you driving?");
+let plannedSpeed = prompt("How fast do you want to drive?");
+let distanceleft = prompt("How many miles do you have left?");
+
+let timeAtCurrentSpeed = (distanceleft/currentSpeed) * 60;
+let timeAtPlannedSpeed = (distanceleft/plannedSpeed) * 60
+let timeSaved = timeAtCurrentSpeed - timeAtPlannedSpeed;
+
+console.log(`At your current speed you will take ${timeAtCurrentSpeed.toFixed(0)} minutes. At your planned speed you will take ${timeAtPlannedSpeed.toFixed(0)} minutes. You will save ${timeSaved.toFixed(0)} minutes.`);
